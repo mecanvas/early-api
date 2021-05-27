cef7d391fb4ef23fdc0966b68dbafea8aee808dc
김창회 <dudnqnfqlc@naver.com>
2021-05-28 04:34:53 +0900

    swagger 설치 (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const { PORT } = process.env;
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('mecanvas')
+    .setDescription('MeCanvas API Docs')
+    .setVersion('1.0')
+    .build();
+
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
+  console.log('server is running', `http://localhost:${PORT}`);
 }
 
 bootstrap();
