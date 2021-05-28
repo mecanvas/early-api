b68be6966fbc5430fd86601483d95cefd0bd217a
김창회 <dudnqnfqlc@naver.com>
2021-05-29 03:53:27 +0900

    canvas router 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CanvasModule } from './canvas/canvas.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    CanvasModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/canvas/canvas.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+// https://velog.io/@suasue/NestJS-AWS-S3-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C
+@Injectable()
+export class CanvasService {
+  uploadImage(): {};
+}
