e54ef07951f8f23c384f835386ad0cf697a09ef3
김창회 <dudnqnfqlc@naver.com>
2021-06-11 23:43:11 +0900

    sql + typeorm 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { CanvasModule } from './canvas/canvas.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './ormConfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot(ormConfig),
     CanvasModule,
   ],
   controllers: [AppController],
